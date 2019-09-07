479707eb17b268161c9da1d5ccfbf4130c9236a2
Bearbear@bear.com <bear@bear.com>
2019-09-07 02:04:05 -0500

    idea: use the pandas module's dateoffset to cleanly deal with dates e.g. leap years

--- timedeltas_start.py
+++ timedeltas_start.py
@@ -6,6 +6,7 @@ from datetime import date
 from datetime import time
 from datetime import datetime
 from datetime import timedelta
+import pandas as pd
 
 # construct a basic timedelta and print it
 print(timedelta(days=365, hours=5, minutes=1))
@@ -35,7 +36,21 @@ xmasday = date(today.year, 12, 25)
 if xmasday < today:
     print("Christmas Day already went by %d days ago" % ((today-xmasday).days))
     xmasday = xmasday.replace(year = today.year+1)
+    print(xmasday.ctime())
 
-# Now calculate the amount of time until April Fool's Day  
+# Now calculate the amount of time until Christmas Day
 time_to_xmasday = xmasday-today
 print("It's just", time_to_xmasday.days, "days until Christmas Day")
+
+# seems like pandas (one of the most popular python data libraries) is good for dealing with time, like:
+
+import pandas as pd # normally you'd put this up top
+                    # you will need to 'pip install pandas' from a terminal prompt to install the pandas python module
+leap_year_day_string = "February 29, 2016"
+leap_year_format_string = "%B %d, %Y"
+
+leap_year_datetime = datetime.strptime(leap_year_day_string, leap_year_format_string).date()
+print("Leap year: " + leap_year_datetime.strftime('%m/%d/%Y, %H:%M:%S'))
+
+next_year_datetime = leap_year_datetime + pd.DateOffset(years=1)
+print("Plus one year: " + next_year_datetime.strftime('%m/%d/%Y, %H:%M:%S'))
